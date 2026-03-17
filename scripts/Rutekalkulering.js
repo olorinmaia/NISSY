e1d0c8ec039c65e719a323a3202942a1424ad9de
Alf Einar Johnsen <114103483+olorinmaia@users.noreply.github.com>
2026-03-17 13:40:29 +0100

    Add ARA pol to Levanger hospital mappings

--- scripts/Rutekalkulering.js
+++ scripts/Rutekalkulering.js
@@ -117,6 +117,7 @@
     "SLS, 7600 Levanger": "Sykehuset Levanger, 7600 Levanger",
     "Geriatrisk pol, 7600 Levanger": "Sykehuset Levanger, 7600 Levanger",
     "Kir post 3, 7600 Levanger": "Sykehuset Levanger, 7600 Levanger",
+    "ARA pol, 7600 Levanger": "Sykehuset Levanger, 7600 Levanger",
     
     // Sykehuset Namsos - avdelinger/poliklinikker
     "Ort D3, 7803 Namsos": "Sykehuset Namsos, 7803 Namsos",
