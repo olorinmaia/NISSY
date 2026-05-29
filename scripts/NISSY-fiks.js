f79e82750a0b9620772c25d8a315f31d37cec84e
olorinmaia <alfeinarj@gmail.com>
2026-05-29 12:26:34 +0200

    Update NISSY-fiks.js

--- scripts/NISSY-fiks.js
+++ scripts/NISSY-fiks.js
@@ -13,7 +13,7 @@
     return;
   }
   window.__nissyMasterScriptInstalled = true;
-  const SCRIPT_VERSION = '4.5.7'; // Versjonsnummer for debugging og fremtidige oppdateringer
+  const SCRIPT_VERSION = '4.5.8'; // Versjonsnummer for debugging og fremtidige oppdateringer
   window.__nissyScriptVersion = SCRIPT_VERSION;
 
   console.log("🚀 Starter NISSY-fiks-script");
