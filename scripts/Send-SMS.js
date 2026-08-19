63340681c0f53c6b6bd435c30ae9e686456ecf87
asa032 <arvid-johan.sandnes@nordlandssykehuset.no>
2026-08-19 08:15:10 +0200

    Update Send-SMS.js

--- scripts/Send-SMS.js
+++ scripts/Send-SMS.js
@@ -438,7 +438,7 @@
         {
           navn: "Bodø lufthavn",
           tekst: () =>
-            `Dette er en melding fra Pasientreiser, meldingen kan ikke besvares med SMS.\n\nFor taxi på bestilt rekvisisjon, ta kontakt med Pasientreiser tlf 05515 når du lander i Bodø. Våre åpningstider er hverdager 08.00 - 16.00. Om du lander utenom vår åpningstid ringer du Nordland taxi tlf 07550.\n\nHa en fin dag.`,
+            `Dette er en melding fra Pasientreiser, meldingen kan ikke besvares med SMS.\n\nFor taxi på bestilt rekvisisjon, ta kontakt med Pasientreiser tlf 05515 når du ankommer Bodø. Våre åpningstider er hverdager 08.00 - 16.00. Om du ankommer utenom vår åpningstid ringer du Nordland taxi tlf 07550.\n\nHa en fin dag.`,
         },
         {
           navn: "Svolvær lufthavn",
